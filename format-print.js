45a277979064856625084e77db449309e0b1c6d7
Paul Bernitz <paulbernitz@web.de>
2018-07-23 19:44:33 +0200

    The user can now specify wether the cells should match the biggest value in their data or to get cut off at the specified size.

--- format-print.js
+++ format-print.js
@@ -19,13 +19,17 @@ let tinyFormat = (function(){
   }
 
   // Combine the block and its value to create a formatted cell.
-  function buildCellFromBlock(block, value){
+  function buildCellFromBlock(block, value, showHidden){
     let col = '|';
     for(let i=0; i<block.length; ++i){ col += block.char; }
     col += '|';
     value = String(value);
+    if(showHidden === false && value.length >= block.length){
+      value = value.substr(0, block.length - 3) + '..';
+    } 
     let startIdx = block.align == 'l' ? 2 : (block.align == 'r' ? block.length - value.length : (col.length / 2) - (value.length / 2));
-    return (col.substr(0, startIdx) + value + col.substr(startIdx + value.length));
+    let cell = col.substr(0, startIdx) + value + col.substr(startIdx + value.length);
+    return cell.slice(0, -1); // -1 to remove the right bounds.
   }
 
   // Attaches columns together in a single string.
@@ -35,38 +39,56 @@ let tinyFormat = (function(){
       for(let j=i; j<columns.length; j += dataArrayLength + 1){
         formatted += columns[j];
       }
-      formatted += '\n';
+      formatted += '|\n';
     }
     return formatted;
+  }
+
+  function fitCellLengthToData(newLength, dataArray, blockComponents){
+    dataArray.forEach(data => {
+      let dataValue = String(data[blockComponents.variable]);
+      if(dataValue.length > blockComponents.length && dataValue.length > newLength){
+        newLength = dataValue.length + 2; // +2 is needed to not overlap the bounds.
+      }
+    });
+    return newLength;
   }
 
 /*
   ___public__________________________________________________________________________
 */
-  myself.formatTable = function(formatString, dataArray){
+  myself.formatTable = function(formatString, dataArray, showHidden=false){
     // @Todo: Use regex to check if the formatting of the formatString is valid.
-    // @Todo: Check if one of the data values is too long for the specified cell width.
 
     // Get all blocks from the formatString.
     let blocks = formatString.split('|').filter(elem => elem.length > 0);
     let parsedBlocks = [];
     
     // Split the blocks into their components.
-    blocks.forEach(block => {
-      parsedBlocks.push(splitBlockComponents(block));
+    blocks.forEach(block => {      
+
+      let blockComponents = splitBlockComponents(block);
+      
+      if(showHidden){ // Fit the cells to the biggest value if showHidden is true.
+        blockComponents.length = fitCellLengthToData(blockComponents.length, dataArray, blockComponents);
+      }
+
+      parsedBlocks.push(blockComponents);
     });
 
     // Build each column.
     let columns = [];
     parsedBlocks.forEach(block => {
-      columns.push(buildCellFromBlock(block, block.label)); // Create Label cell.
+      columns.push(buildCellFromBlock(block, block.label, showHidden)); // Create Label cell.
       dataArray.forEach(data => { // Create data cells.
-        columns.push(buildCellFromBlock(block, data[block.variable]));
+        columns.push(buildCellFromBlock(block, data[block.variable], showHidden));
       });
     });
 
     // Attach each column to the final string.
-    return buildStringFromColumns(columns, dataArray.length); 
+    let finalString = buildStringFromColumns(columns, dataArray.length);
+
+    return finalString; 
   }
 
   return myself;
